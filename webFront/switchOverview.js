629ba2d49431cc4496df9a65c8ea12d6712c2c91
bertmad3400 <skrivtilbertram@gmail.com>
2021-06-21 21:56:46 +0200

    Add the needed JS for changing the small article overview

---
+++ webFront/switchOverview.js
@@ -0,0 +1,41 @@
+// Will contain the following variables: imageURLs, descriptions, titles and articleURLs 
+$variables
+
+// Counter for knowing which article has been reached when pressing the left or right arrow
+currentCounter = 0
+
+// Previewnumber is determining which of the preview windows is changed (0 for the left, 1 for the on in the middle and 2 for the right) and detailNumber is determining which entry in the arrays storing the title, sourceurls, imageurls and descriptions is used
+function changeIndividualPreview(previewNumber, detailNumber){
+
+
+	document.querySelectorAll('a.article-link')[previewNumber].setAttribute("href", articleURLs[detailNumber])
+	document.querySelectorAll('img.article-image')[previewNumber].setAttribute("src", imageURLs[detailNumber])
+	document.querySelectorAll('div.desc h2')[previewNumber].textContent = titles[detailNumber]
+	document.querySelectorAll('div.desc p')[previewNumber].textContent = descriptions[detailNumber]
+
+}
+
+// The currentNumber is variable for telling how far the user has scrolled right or left using the arrows (will often just be currentCounter)
+function changePreviews(currentNumber){
+	for (let i = 0; i < 3; i++){
+		changeIndividualPreview(i, ((i + currentNumber) % (imageURLs.length - 1)))
+	}
+}
+
+function arrowPress(left) {
+	if (left == true) {
+		currentCounter = currentCounter - 3
+		
+		if (currentCounter < 0) {
+			currentCounter = (imageURLs.length - 1) + currentCounter
+		}
+
+		changePreviews(currentCounter)
+
+	} else {
+		
+		currentCounter = (currentCounter + 3) % (imageURLs.length - 1)
+		changePreviews(currentCounter)
+
+	}
+}
