2ec0900fb77db2699aeec4dcc2b6a9664b8eec39
bertmad3400 <skrivtilbertram@gmail.com>
2021-06-22 11:15:01 +0200

    Remember to quote image URLs before adding to CSS

--- main.py
+++ main.py
@@ -278,12 +278,12 @@ def constructArticleOverview(OGTags):
         # OSINTwProfile is used when scraping the website, to know what profile is associated with the article the user choose
         URL = article['url'] + ('&' if parse.urlparse(article['url']).query else '?') + "OSINTwProfile=" + article['profile']
         HTML += '<article id="card-' + str(i) + '"><a href="' + URL + '"><h1>' + article['title'] + '</h1></a></article>\n'
-        CSS += '#card-' + str(i) + '::before { background-image: url(' + article['image'] + ');}\n'
+        CSS += '#card-' + str(i) + '::before { background-image: url("' + article['image'] + '");}\n'
         JSList[0].append(URL)
         JSList[1].append(article['image'])
         JSList[2].append(article['title'])
         JSList[3].append(article['description'])
-    
+
     for currentJSList in JSList:
         JS += 'const ' + currentJSList.pop(0) + ' = [ "' + currentJSList.pop(0) + '"' + "".join([(', "' + element + '"') for element in currentJSList]) + ' ]\n'
 
