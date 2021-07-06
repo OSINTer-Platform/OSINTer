c7ae04e3d433285ceae348820f5c594bb236df13
bertmad3400 <skrivtilbertram@gmail.com>
2021-07-06 17:17:03 +0200

    Add script for scraping and saving articles
    Added a script that will scrape the article overview of all the
    newssites present in the profile folder, scrape and save the OG tags in
    a database for all those that haven't been scraped before, and then
    scrape the whole article, putting it in a markdown format in the
    articles folder inside a folder named the same as the profile.

---
+++ scrapeAndStore.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+# Used for creating a connection to the database
+import psycopg2
+
+# Used for loading the profile
+import json
+
+from OSINTmodules.OSINTprofiles import getProfiles
+from OSINTmodules import *
+
+
+def fromURLToMarkdown(URL, currentProfile, MDFilePath="./"):
+
+    # Scrape the whole article source based on how the profile says
+    if currentProfile['scraping']['type'] == "no-action":
+        articleSource = OSINTscraping.scrapePageDynamic(URL)
+    else:
+        raise Exception(profile['source']['name'] + " apparently didn't have a specified way of scraping the articles autonomously, exiting.")
+
+    # Gather the needed information from the article
+    articleDetails, articleContent, articleClearText = OSINTextract.extractAllDetails(currentProfile, articleSource)
+
+    # Generate the tags
+    articleTags = OSINTtext.generateTags(OSINTtext.cleanText(articleClearText))
+
+    # Create the markdown file
+    MDFileName = OSINTfiles.createMDFile(currentProfile['source']['name'], URL, articleDetails, articleContent, articleTags, MDFilePath)
+
+    return MDFileName
+
+
+def main():
+    # Connecting to the database
+    conn = psycopg2.connect("dbname=osinter user=postgres")
+
+    articleURLLists = OSINTscraping.gatherArticleURLs(getProfiles())
+    OGTagCollection = OSINTtags.collectAllOGTags(articleURLLists)
+
+    # Writting the OG tags to the database and finding those that haven't already been scraped
+    articleCollection = OSINTdatabase.writeOGTagsToDB(conn, OGTagCollection, "articles")
+
+    # Looping through the list of articles from specific news site in the list of all articles from all sites
+    for articleList in articleCollection:
+        currentProfileName = articleList.pop(0)
+
+        # Making sure the folder for storing the markdown files for the articles in exists, will throw exception if not
+        OSINTmisc.createNewsSiteFolder(currentProfileName)
+
+        # Loading the profile for the current website
+        currentProfile = json.loads(getProfiles(currentProfileName))
+
+        # Creating the path to the article for the news site
+        articlePath = "./articles/{}/".format(currentProfileName)
+
+        for articleURL in articleList:
+            fromURLToMarkdown(articleURL, currentProfile, articlePath)
+
+main()
