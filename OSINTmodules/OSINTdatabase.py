198e39dd7b84d6ddc4ad1115393443d21d882cc2
bertmad3400 <skrivtilbertram@gmail.com>
2021-07-06 10:25:10 +0200

    Add code for writting collection of OG tags to database

--- OSINTmodules/OSINTdatabase.py
+++ OSINTmodules/OSINTdatabase.py
@@ -27,3 +27,25 @@ def createTable(connection, tableName, tableContentList):
             connection.commit()
         else:
             print("Table \"%s\" already exists, skipping it for now" % tableName.lower())
+
+# Function for writting OG tags to database
+def writeOGTagsToDB(connection, OGTags, tableName):
+    # Making sure the tablename is in all lowercase
+    tableName = tableName.lower()
+    # List to hold all the urls that haven't been scraped and saved in the database before so the whole article can be scraped
+    newUrls = list()
+    with connection.cursor() as cur:
+        for newsSite in OGTags:
+            # Looping through each collection of tags
+            for tags in OGTags[newsSite]:
+                # Checking if the article is already stored in the database using the URL as that is probably not going to change and is uniqe
+                cur.execute("SELECT exists (SELECT 1 FROM {} WHERE url = %s);".format(tableName), (tags['url'],))
+                if cur.fetchall()[0][0] == False:
+                    # Adding the url to list of new articles since it was not found in the database
+                    newUrls.append(tags['url'])
+                    insertQuery = "INSERT INTO {} (title, description, url, image_url, profile) VALUES (%s, %s, %s, %s, %s);".format(tableName)
+                    insertParameters = (tags['title'], tags['description'], tags['url'], tags['image'], newsSite)
+                    cur.execute(insertQuery, insertParameters)
+    connection.commit()
+    # Return the list of urls not already in the database so they can be scraped
+    return newUrls
