15c5ae3d8dd4bc8c169127e30a6e3cc77bd22b36
bertmad3400 <skrivtilbertram@gmail.com>
2021-07-06 17:05:34 +0200

    Add file for initializing backend
    Add a python script which will take the steps necessary to go from
    having an empty postgresql database to the needed table in a new
    database along with the needed local folder

---
+++ initiateScrapingBackend.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+# Used for creating a connection to the database
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+import os
+from pathlib import Path
+
+from OSINTmodules import OSINTdatabase
+
+
+if not os.path.isdir(Path("./articles")):
+    try:
+        os.mkdir(Path("./articles"))
+    except:
+        raise Exception("The folder needed for creating storing the markdown files representing the articles couldn't be created, exiting")
+
+# Connecting to the database
+conn = psycopg2.connect("user=postgres")
+
+# Needed ass create database cannot be run within transaction
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+# Creating a new database
+with conn.cursor() as cur:
+    cur.execute("CREATE DATABASE osinter;")
+
+conn.close()
+
+# Connecting to the newly created database
+conn = psycopg2.connect("dbname=osinter user=postgres")
+
+# Making sure the database has gotten the needed table(s)
+OSINTdatabase.initiateArticleTable(conn)
